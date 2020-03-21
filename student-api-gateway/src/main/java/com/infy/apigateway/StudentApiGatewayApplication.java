49d78ce7b38ad69899a0dac62f37d0215012ef31
rohit <rohit@rohit-Vostro-1015>
2020-03-21 16:23:58 +0530

    studetn-api-gateway without JWT authentication.

---
+++ student-api-gateway/src/main/java/com/infy/apigateway/StudentApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.infy.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentApiGatewayApplication.class, args);
+	}
+
+}
